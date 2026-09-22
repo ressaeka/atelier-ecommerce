21d2046898c09f84869817ec88a28964decc865f
ressaeka <reysaeka10@gmail.com>
2026-09-22 22:21:38 +0700

    chore: clean unused order templates

--- atelier-api/src/order/dto/update-order.dto.ts
+++
@@ -1,4 +0,0 @@
-// import { PartialType } from '@nestjs/swagger';
-// import { CreateOrderDto } from './create-order.dto.js';
-
-// export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
